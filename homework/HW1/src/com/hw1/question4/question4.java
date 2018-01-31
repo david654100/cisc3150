2f3d6e8d01b908086babc898fe68651c60b3c88f
david654100 <david654100@gmail.com>
2018-01-31 16:18:14 -0500

    1

--- homework/HW1/src/com/hw1/question4/question4.java
+++ homework/HW1/src/com/hw1/question4/question4.java
@@ -4,8 +4,9 @@ import java.util.*;
 
 public class question4{
     public static void main(String[] args)  {
-        Scanner instream = new Scanner(System.in);
 
+        Scanner instream = new Scanner(System.in);
+        System.out.println("Numbers:");
         while(instream.hasNextInt()){
             System.out.println(instream.nextInt());
         }
